1c427a3d786d100f312c27628274722723edc903
chenchenyangll <ccy-8888@hotmail.com>
2015-01-10 13:28:26 +0800

    handle redirect

--- wikia.js
+++ wikia.js
@@ -1,22 +1,18 @@
 module.exports = (function() {
   var request = require('request');
-  // var http = require('http');
-  // var _ = require('underscore');
   var BASE = 'http://zh.asoiaf.wikia.com';
-  /*
-  var options = {
-    hostname: BASE, 
-    path: '', 
-    method: 'GET', 
-    agent: false
-  };
-  */
+
   var wikia = function() {
   };
   
   wikia.prototype = {
     /*
      * Get page info according to its title
+     *
+     * options may be a string (page title) or an object, {
+     *   title: page title, 
+     *   redirect: a boolean indicates whether to retrieve redirected page 
+     * }
      *
      * callback receives one object argument, defined as, {
      *   url: page url, 
@@ -25,7 +21,16 @@ module.exports = (function() {
      *   picurl: picture selected by api 
      * }
      */
-    info: function(title, callback) {
+    info: function(options, callback) {
+      var that = this;
+      var title = '';
+      var redirect = true;
+      if (typeof(options) == 'object') {
+        title = options.title;
+        redirect = options.redirect;
+      } else {
+        title = options;
+      }
       var url = BASE + '/api/v1/Articles/Details?abstract=500&width=200&height=200&titles=' + title;
       request.get(url, function(err, res, body) {
         if (!err && res.statusCode == 200) {
@@ -38,7 +43,17 @@ module.exports = (function() {
           }
           // if (article.title == title) {
           // }
-          if (article) {          
+          if (article) { 
+            if (redirect) {
+              var abstr = article['abstract'];
+              var lowAbstr = abstr.toUpperCase();
+              if (lowAbstr.startWith('REDIRECT') || lowAbstr.startWith('重定向')) {
+                var index = abstr.indexOf(' ') + 1;
+                title = abstr.substring(index);
+                that.info(title, callback);
+                return;
+              }
+            }
             callback({
               'url': BASE + article.url, 
               // 'lastEditor': article.revision.user, 
@@ -46,6 +61,7 @@ module.exports = (function() {
               'picurl': article.thumbnail
             });
           } else {
+            // TODO: try search
             callback();
           }
         } else if (err) {
@@ -54,44 +70,16 @@ module.exports = (function() {
           console.log('Response status: ' + res.statusCode);
         }
       });
-    
-    
-      /*
-      var op = _.clone(options);
-      op.path = '/api/v1/Articles/Details?abstract=500&width=200&height=200&titles=' + title;
-      
-      var result = '';
-      var req = http.request(op, function(res) {
-        res.on('data', function(data) {
-          result += data;
-        }).on('end', function() {
-          result = JSON.parse(result);
-          var items = result.items && result.items;
-          var article = null;
-          for (var id in items) {
-            article = items[id];
-            break;
-          }
-          // if (article.title == title) {
-          // }
-          if (article) {          
-            callback({
-              'url': BASE + article.url, 
-              // 'lastEditor': article.revision.user, 
-              'abstract': article['abstract'], 
-              'picurl': article.thumbnail
-            });
-          } else {
-            callback();
-          }
-        });
-      });
-      req.on('error', function(err) {
-        console.log(err);
-      });
-      req.end();
-      */
     }
+  };
+  
+  /*
+   * Utility functions used by wikia.js
+   */
+  String.prototype.startWith = function(str) {
+    if (str == null || str == '' || this.length == 0 || str.length > this.length) return false;
+    if (this.substr(0, str.length) ==str) return true; else return false;
+    return true;
   };
   
   return wikia;
